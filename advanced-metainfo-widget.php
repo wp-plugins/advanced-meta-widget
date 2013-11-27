405d37f1d73718c028d662966b87fb7f80c3c997
sjiamnocna <sjiamnocna@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-11-27 18:06:55 +0000

    updating to 0.9.2
    git-svn-id: https://plugins.svn.wordpress.org/advanced-meta-widget/trunk@811539 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- advanced-metainfo-widget.php
+++ advanced-metainfo-widget.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Advanced meta widget
  * Description: Widget for displaying Wordpress info - Login/out, Admin, RSS feeds and Link to wordpress.org extended by few functions as Loginout redirect...
- * Version: 0.9.1
+ * Version: 0.9.2
  * Text Domain: metawidget
  * Domain Path: /langs
  * Author: Sjiamnocna
@@ -30,6 +30,7 @@ class adv_meta_info extends WP_Widget {
                 $use_redir=isset($instance['use_loginout_redir']) ? $instance['use_loginout_redir'] : false;
                 $show_wpreglink=isset($instance['show_wpreglink']) ? $instance['show_wpreglink'] : false;
                 $show_postrsslink=isset($instance['show_postrsslink']) ? $instance['show_postrsslink'] : false;
+                $postrsslink_type=isset($instance['use_rss_format']) ? $instance['use_rss_format'] : '';
                 $show_commentrsslink=isset($instance['show_commentrsslink']) ? $instance['show_commentrsslink'] : false;
                 $show_wordpressorglink=isset($instance['show_wordpressorglink']) ? $instance['show_wordpressorglink'] : false;
                 $show_profilelink=isset($instance['show_profilelink']) ? $instance['show_profilelink'] : false;
@@ -61,7 +62,7 @@ class adv_meta_info extends WP_Widget {
                 }
                 if($show_postrsslink){
                     echo $each_entry['before'];
-                    echo'<a href="'.get_bloginfo('rss2_url').'" alt="'.__('Posts RSS feed','metawidget').'">'.__('Posts RSS feed','metawidget').'</a>';
+                    echo'<a href="'.get_bloginfo($postrsslink_type).'" alt="'.__('Posts RSS feed','metawidget').'">'.__('Posts RSS feed','metawidget').'</a>';
                     echo $each_entry['after'];
                 }
                 if($show_commentrsslink){
@@ -74,6 +75,14 @@ class adv_meta_info extends WP_Widget {
                     echo'<a href="http://wordpress.org/" alt="'.__('link to Wordpress.org','metawidget').'">'.'Wordpress.org'.'</a>';
                     echo $each_entry['after'];
                 }
+                echo'<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+<input type="hidden" name="cmd" value="_s-xclick">
+<input type="hidden" name="encrypted" value="-----BEGIN PKCS7-----MIIHLwYJKoZIhvcNAQcEoIIHIDCCBxwCAQExggEwMIIBLAIBADCBlDCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20CAQAwDQYJKoZIhvcNAQEBBQAEgYAEk6PIAHYhukJawgs5fDFrT4GNpnivH1rbZAwuscbm1NmZ1C+EW4RWLHLJbu/cpseYpbIcDGHckVac5if5Ir0+mMpAsxc9Q4lNHg3O/fPUgF8f+FkVktv/4XBV6dkHOsqpc65J6J5kpnNSgiA+guIQnnPjGWNBI4EztHuxu4+9gTELMAkGBSsOAwIaBQAwgawGCSqGSIb3DQEHATAUBggqhkiG9w0DBwQIBI2AOBirKaSAgYhtTNur/iBV96+neRbMGsRPj4qV/qg6jJ8FHt7fMyZFNGe2OALFOLXhs4QKzZ7kqwZyRQrEgEUqfnhp3ZD2az7aleNcUdnMwskBcUlkPvCTBjbWLl7x40dDXyKgLuLf72xLUQ4PmUCCukeuGC4HL6gx/T3ApMIpI2ELBnZOO1DAHHUml6jr8h5joIIDhzCCA4MwggLsoAMCAQICAQAwDQYJKoZIhvcNAQEFBQAwgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tMB4XDTA0MDIxMzEwMTMxNVoXDTM1MDIxMzEwMTMxNVowgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDBR07d/ETMS1ycjtkpkvjXZe9k+6CieLuLsPumsJ7QC1odNz3sJiCbs2wC0nLE0uLGaEtXynIgRqIddYCHx88pb5HTXv4SZeuv0Rqq4+axW9PLAAATU8w04qqjaSXgbGLP3NmohqM6bV9kZZwZLR/klDaQGo1u9uDb9lr4Yn+rBQIDAQABo4HuMIHrMB0GA1UdDgQWBBSWn3y7xm8XvVk/UtcKG+wQ1mSUazCBuwYDVR0jBIGzMIGwgBSWn3y7xm8XvVk/UtcKG+wQ1mSUa6GBlKSBkTCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb22CAQAwDAYDVR0TBAUwAwEB/zANBgkqhkiG9w0BAQUFAAOBgQCBXzpWmoBa5e9fo6ujionW1hUhPkOBakTr3YCDjbYfvJEiv/2P+IobhOGJr85+XHhN0v4gUkEDI8r2/rNk1m0GA8HKddvTjyGw/XqXa+LSTlDYkqI8OwR8GEYj4efEtcRpRYBxV8KxAW93YDWzFGvruKnnLbDAF6VR5w/cCMn5hzGCAZowggGWAgEBMIGUMIGOMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExFjAUBgNVBAcTDU1vdW50YWluIFZpZXcxFDASBgNVBAoTC1BheVBhbCBJbmMuMRMwEQYDVQQLFApsaXZlX2NlcnRzMREwDwYDVQQDFAhsaXZlX2FwaTEcMBoGCSqGSIb3DQEJARYNcmVAcGF5cGFsLmNvbQIBADAJBgUrDgMCGgUAoF0wGAYJKoZIhvcNAQkDMQsGCSqGSIb3DQEHATAcBgkqhkiG9w0BCQUxDxcNMTMxMTI3MTY1MDUzWjAjBgkqhkiG9w0BCQQxFgQU+YVaK5pLuNKv9GWHX8ieHaujFx8wDQYJKoZIhvcNAQEBBQAEgYCYCFDlThpYPL9yD83z1iobcOFFxVX9ndbTdDkKZ5bzpL0BZLTuvBjWt97iwyOeWGL+zPj7vO0078EFuH3WBBHhXNkNeW39AB9PeLv9dMsfqvjeRg/PJYiQo8f4t/p5R9azKe1cNuT+xixrG7hbbJJVE5RcVzB8k05zpN2otgXJ8Q==-----END PKCS7-----
+">
+<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+</form>
+';
                 echo $whole_widget['after'];
 		echo $after_widget;
 	}
@@ -89,10 +98,11 @@ class adv_meta_info extends WP_Widget {
                 $instance['show_wordpressorglink']=$new_instance['show_wordpressorglink'];
                 $instance['show_profilelink']=$new_instance['show_profilelink'];
                 $instance['show_as_element']=$new_instance['show_as_element'];
+                $instance['use_rss_format']=$new_instance['use_rss_format'];
 		return $instance;
 	}
 	function form($instance) {
-		$defaults = array('title'=>__('Advanced meta info Widget','metawidget'),'show_as_element'=>'li','show_admin_link'=>'yes','show_loginout'=>'yes','use_loginout_redir'=>false,'show_profilelink'=>false,'show_postrsslink'=>'yes','show_commentrsslink'=>false,'show_wordpressorglink'=>'yes');
+		$defaults = array('title'=>__('Advanced meta info Widget','metawidget'),'show_as_element'=>'li','show_admin_link'=>'yes','show_loginout'=>'yes','use_loginout_redir'=>false,'show_profilelink'=>false,'show_postrsslink'=>'yes','use_rss_format'=>'rss_url','show_commentrsslink'=>false,'show_wordpressorglink'=>'yes','wporglink_lang'=>'nolang');
 		$instance = wp_parse_args($instance, $defaults );
                 ?>
             <div id="options" style="margin:0px 0 15px 0;padding:0px;">
@@ -124,7 +134,15 @@ class adv_meta_info extends WP_Widget {
                 <p style="margin:0px;">
                     <input class="checkbox" type="checkbox" value="yes" id="<?php echo $this->get_field_id('show_postrsslink'); ?>" name="<?php echo $this->get_field_name('show_postrsslink'); ?>" <?php checked( $instance['show_postrsslink'],'yes'); ?> /> 
                     <label for="<?php echo $this->get_field_id('show_postrsslink'); ?>"><?php _e('Display post RSS feed link','metawidget'); ?></label><br/>
+                    
+                    <select style="margin-left:8%;" id="<?php echo $this->get_field_id('use_rss_format'); ?>" name="<?php echo $this->get_field_name('use_rss_format'); ?>">
+                        <option value="atom_url" <?php selected($instance['use_rss_format'],'atom_url'); ?>><?php _e('RSS Atom','metawidget'); ?>
+                        <option value="rdf_url" <?php selected($instance['use_rss_format'],'rdf_url'); ?>><?php _e('RDF','metawidget'); ?>
+                        <option value="rss_url" <?php selected($instance['use_rss_format'],'rss_url'); ?>><?php _e('RSS','metawidget'); ?>
+                        <option value="rss2_url" <?php selected($instance['use_rss_format'],'rss2_url'); ?>><?php _e('RSS2','metawidget'); ?>
+                    </select>
                 </p>
+                
                 <p style="margin:0px;">
                     <input class="checkbox" type="checkbox" value="yes" id="<?php echo $this->get_field_id('show_commentrsslink'); ?>" name="<?php echo $this->get_field_name('show_commentrsslink'); ?>" <?php checked( $instance['show_commentrsslink'],'yes'); ?> /> 
                     <label for="<?php echo $this->get_field_id('show_commentrsslink'); ?>"><?php _e('Display comments RSS feed link','metawidget'); ?></label><br/>
@@ -132,6 +150,11 @@ class adv_meta_info extends WP_Widget {
                 <p style="margin:0px;">
                     <input class="checkbox" type="checkbox" value="yes" id="<?php echo $this->get_field_id('show_wordpressorglink'); ?>" name="<?php echo $this->get_field_name('show_wordpressorglink'); ?>" <?php checked( $instance['show_wordpressorglink'],'yes'); ?> /> 
                     <label for="<?php echo $this->get_field_id('show_wordpressorglink'); ?>"><?php _e('Display link to Wordpress.org','metawidget'); ?></label><br/>
+                    <select style="margin-left:8%;" id="<?php echo $this->get_field_id('wporglink_lang'); ?>" name="<?php echo $this->get_field_name('wporglink_lang'); ?>">
+                        <option value="nolang" <?php selected($instance['wporglink_lang'],'nolang'); ?>><?php _e('Wordpress.org','metawidget'); ?>
+                        <option value="ru" <?php selected($instance['wporglink_lang'],'ru'); ?>><?php _e('Russian','metawidget'); ?>
+                        <option value="cs" <?php selected($instance['wporglink_lang'],'sc'); ?>><?php _e('Czech','metawidget'); ?>
+                    </select>
                 </p>
                 <p style="margin:15px 5px 5px 25px;">
                     <label for="<?php echo $this->get_field_id('show_as_element'); ?>"><?php _e('Display items as','metawidget'); ?></label><br/>
--- readme.txt
+++ readme.txt
@@ -1,9 +1,10 @@
 === Advanced meta widget ===
-Contributors: Sjiamnocna
-Tags: meta, widget, loginout, redirect, info, admin, user
+Contributors: sjiamnocna
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=65SS8NS48FPFQ&lc=CZ&item_name=%c5%a0imon%20Jan%c4%8da&currency_code=CZK&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHosted
+Tags: meta, widget, loginout, redirect, info, admin, user, wordpress.org, language, RSS
 Requires at least: 2.7
-Tested up to: 3.5.2
-Stable tag: 2.5.8
+Tested up to: 3.7.1
+Stable tag: 0.8
 License: GPLv2 or later
 
 Advanced meta widget allows you to chose, what you want to display in meta widget and adds many other features to manage it.
@@ -24,6 +25,10 @@ Unpack archive contents into plugin folder (/wp-content/plugins/) and activate i
 
 == Changelog ==
 
+= 0.9.2 =
+*Added option to chose what type of RSS to use for posts RSS link and option to change the wordpress.org url for some languages.
+= 0.9.1 =
+*Some bux fixes
 = 0.9 =
 *Added option to chose what HTML elements will be used for generating widget - you can chose if you want div and paragraph or list items
 = 0.8.1 =
